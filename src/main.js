2035d10a5c16a85eb400ba9d19095b422cf88f67
João J <etiontdn@gmail.com>
2025-01-29 19:26:26 -0300

    Adiciona importação e uso do componente BottomNav no App.vue

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import '@mdi/font/css/materialdesignicons.css'
+
 
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -12,6 +15,13 @@ import router from './router'
 
 
 const vuetify = createVuetify({
+    icons: {
+        defaultSet: 'mdi',
+        aliases,
+        sets: {
+          mdi,
+        },
+      },
     components,
     directives,
   })
